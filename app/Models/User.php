a8407f8ef52744754bb87d35d3b008ffb9bfbe7c
CristianSG2 <alum.cristian.sanchez@gmail.com>
2025-10-01 17:29:52 +0200

    chore(base): asegurar fillable/hidden en User y eliminar ruta duplicada '/'

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,22 +12,14 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    // Campos asignables en masa al registrar
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password', // se hashea gracias al cast 'hashed'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    // Ocultar al serializar
     protected $hidden = [
         'password',
         'remember_token',
@@ -42,7 +34,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password' => 'hashed', // Laravel 11: hashea al asignar
         ];
     }
 }
